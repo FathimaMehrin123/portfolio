de4a991101c2fb0f3e6e0c8dcaf7258a218b0781
Fathima Mehrin <fathimamehrinv@gmail.com>
2025-08-07 14:31:47 +0530

    Second file added

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_website/screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage(),title: 'mywebpage');
+  }
+}
---
+++ lib/screens/core/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final Color primarycolor = Color(0xff233353);
+
+final Color seccolor = Color(0xffe5a38c);
+final Color buttonclr = Color(0xffFF7069);
